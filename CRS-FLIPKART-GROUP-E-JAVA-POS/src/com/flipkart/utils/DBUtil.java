ff72809928ee7d9f0c0326a83f5acfbd5a63cc2d
rishabhraj1506 <rishabh.raj@flipkart.com>
2024-08-12 10:51:17 +0530

    utils

--- CRS-FLIPKART-GROUP-E-JAVA-POS/src/com/flipkart/utils/DBUtil.java
+++ CRS-FLIPKART-GROUP-E-JAVA-POS/src/com/flipkart/utils/DBUtil.java
@@ -1,5 +1,6 @@
 package com.flipkart.utils;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
@@ -17,10 +18,24 @@ private static Connection connection = null;
             return connection;
         else {
             try {
-                connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/crsapp","root","Bai4ish4!@#%");
+            	Properties prop = new Properties();
+                InputStream inputStream = new FileInputStream("/Users/rishabh.raj/Documents/workspace-spring-tool-suite-4-4.24.0.RELEASE/CRS/src/config.properties");
+                prop.load(inputStream);
+                String driver = prop.getProperty("driver");
+                String url = prop.getProperty("url");
+                String user = prop.getProperty("user");
+                String password = prop.getProperty("password");
+                Class.forName(driver);
+                connection = DriverManager.getConnection(url, user, password);
             } catch (SQLException e) {
                 e.printStackTrace();
-            } 
+            } catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} 
             return connection;
         }
 	}
